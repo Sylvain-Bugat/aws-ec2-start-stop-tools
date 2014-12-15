0d3125b58018e76aa328aed369f0e596ee2b8f75
Sylvain Bugat <sylvain.bugat@laposte.net>
2014-12-15 23:42:08 +0100

    Adding logs and comments

--- src/main/java/com/github/sbugat/ec2tools/Configuration.java
+++ src/main/java/com/github/sbugat/ec2tools/Configuration.java
@@ -12,12 +12,27 @@ import org.apache.commons.configuration.SubnodeConfiguration;
 import org.slf4j.ext.XLogger;
 import org.slf4j.ext.XLoggerFactory;
 
+/**
+ * Configuration loading class
+ *
+ * @author Sylvain Bugat
+ *
+ */
 public class Configuration {
 
+	/**
+	 * SLF4J Xlogger
+	 */
 	private static final XLogger log = XLoggerFactory.getXLogger( Configuration.class );
 
+	/**
+	 * Configuration file name
+	 */
 	public static final String CONFIGURATION_FILE_NAME ="ec2tools.ini";
 
+	/**
+	 * Loaded configuration
+	 */
 	private final Map<String,List<InstanceOrder>> configuredSections = new HashMap<String, List<InstanceOrder>>();
 
 	public Configuration() throws ConfigurationException {
@@ -29,6 +44,8 @@ public class Configuration {
 		//Sections loading
 		for( final String section : hierarchicalINIConfiguration.getSections() ) {
 
+			log.info( "Loading section: {}", section );
+
 			final List<InstanceOrder> sectionList = new ArrayList<InstanceOrder>();
 			configuredSections.put( section, sectionList );
 
@@ -38,8 +55,11 @@ public class Configuration {
 			while( iterator.hasNext() ) {
 
 				final String instanceId = iterator.next();
+				log.trace( "Loading configuration for instanceId: {} -> order: {}", instanceId, subnodeConfiguration.getString( instanceId ) );
 				sectionList.add( new InstanceOrder( instanceId, subnodeConfiguration.getString( instanceId ) ) );
 			}
+
+			log.info( "section {} loaded with {} orders", section, sectionList.size() );
 		}
 
 		log.exit();
