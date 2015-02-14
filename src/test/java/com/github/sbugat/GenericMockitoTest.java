afc9c5cf2e1a913574e76eb3a5edc497150e10b5
Sylvain Bugat <sylvain.bugat@laposte.net>
2015-02-14 14:46:31 +0100

    Add comments and minor changes

--- src/test/java/com/github/sbugat/GenericMockitoTest.java
+++ src/test/java/com/github/sbugat/GenericMockitoTest.java
@@ -15,28 +15,44 @@ import org.mockito.Mockito;
  */
 public abstract class GenericMockitoTest {
 
+	/**
+	 * Scan all instance field to get all ones with @Mock annotation and call verifyNoMoreInteractions on them.
+	 * 
+	 * @throws IllegalArgumentException reflexion exception when acceding fields
+	 * @throws IllegalAccessException reflexion exception when acceding fields
+	 */
 	@After
-	public void verifyNoMoreInteractionsAllMocks() throws IllegalArgumentException, IllegalAccessException {
+	public final void verifyNoMoreInteractionsAllMocks() throws IllegalArgumentException, IllegalAccessException {
 
+		// Get all class from the test class to GenericMockitoTest class
 		Class<? extends GenericMockitoTest> currentClass = this.getClass();
 		while (null != currentClass) {
 
+			// Scan all fields of the current class
 			for (final Field field : currentClass.getDeclaredFields()) {
 
+				// Check if the current field has the @Mock annotation
 				final Annotation annotation = field.getAnnotation(Mock.class);
-
 				if (null != annotation) {
 
+					// Get the mock object to check no more or no interactions
+					final Object mockObject;
 					if (field.isAccessible()) {
-						Mockito.verifyNoMoreInteractions(field.get(this));
-					} else {
+						mockObject = field.get(this);
+					}
+					// In the case of a unaccessible field (private, protected or package), set it accessible temporary
+					else {
 						field.setAccessible(true);
-						Mockito.verifyNoMoreInteractions(field.get(this));
+						mockObject = field.get(this);
 						field.setAccessible(false);
 					}
+
+					Mockito.verifyNoMoreInteractions(mockObject);
 				}
 			}
 
+			// Get the super class of the current class
+			// An unchecked cast is needed because it's not possible to check generic type at runtime
 			@SuppressWarnings("unchecked")
 			final Class<? extends GenericMockitoTest> uncheckedCurrentClass = (Class<? extends GenericMockitoTest>) currentClass.getSuperclass();
 			currentClass = uncheckedCurrentClass;
