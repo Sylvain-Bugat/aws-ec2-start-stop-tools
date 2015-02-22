ee6d149cdc3b0b881809306fbf7bdc54ead64302
Sylvain Bugat <sylvain.bugat@laposte.net>
2015-02-22 19:37:04 +0100

    Add equals

--- src/main/java/com/github/sbugat/ec2tools/InstanceOrder.java
+++ src/main/java/com/github/sbugat/ec2tools/InstanceOrder.java
@@ -1,8 +1,8 @@
 package com.github.sbugat.ec2tools;
 
 /**
- * Loaded instance order
- *
+ * Loaded instance order.
+ * 
  * @author Sylvain Bugat
  */
 public class InstanceOrder {
@@ -11,13 +11,34 @@ public class InstanceOrder {
 
 	public final OrderType orderType;
 
-	public InstanceOrder( final String instanceIdArg, final String orderTypeArg ){
+	public InstanceOrder(final String instanceIdArg, final String orderTypeArg) {
 
 		instanceId = instanceIdArg;
-		orderType = OrderType.valueOf( orderTypeArg );
+		orderType = OrderType.valueOf(orderTypeArg);
 	}
 
+	@Override
 	public String toString() {
 		return instanceId + ':' + orderType;
+	}
+
+	@Override
+	public boolean equals(final Object object) {
+
+		if (this == object) {
+			return true;
+		}
+
+		if (object == null || !InstanceOrder.class.isInstance(object)) {
+			return false;
+		}
+
+		final InstanceOrder instanceOrder = (InstanceOrder) object;
+
+		if (instanceId != null && !instanceId.equals(instanceOrder.instanceId) || orderType != instanceOrder.orderType) {
+			return false;
+		}
+
+		return true;
 	}
 }
