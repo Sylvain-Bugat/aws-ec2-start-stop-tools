5311850356a5a9b2f7333081fd2030303fee190e
Sylvain Bugat <sylvain.bugat@laposte.net>
2015-04-08 00:00:49 +0200

    Checkstyle fixes

--- src/main/java/com/github/sbugat/ec2tools/EC2StartStopMain.java
+++ src/main/java/com/github/sbugat/ec2tools/EC2StartStopMain.java
@@ -7,7 +7,6 @@ import com.github.sbugat.ec2tools.launcher.MainLauncher;
 import com.github.sbugat.ec2tools.service.MainService;
 
 /**
- * Main class of start-stop EC2 tools.
  * 
  * @author Sylvain Bugat
  * 
@@ -15,25 +14,34 @@ import com.github.sbugat.ec2tools.service.MainService;
 public final class EC2StartStopMain {
 
 	/** SLF4J XLogger. */
-	private static final XLogger log = XLoggerFactory.getXLogger(EC2StartStopMain.class);
+	private static final XLogger LOG = XLoggerFactory.getXLogger(EC2StartStopMain.class);
 
+	/**
+	 * Private constructor.
+	 */
 	private EC2StartStopMain() {
 		throw new UnsupportedOperationException();
 	}
 
-	public static void main(final String args[]) throws Exception {
+	/**
+	 * Starting method of the of start-stop EC2 tools.
+	 * 
+	 * @param args program arguments
+	 * @throws Exception thrown when the program exit with an error
+	 */
+	public static void main(final String[] args) throws Exception {
 
-		log.entry((Object[]) args);
-		log.info("Start of EC2StartStop tools");
+		LOG.entry((Object[]) args);
+		LOG.info("Start of EC2StartStop tools");
 
 		try {
 			MainLauncher.launcher(MainService.class, args);
-			log.info("End of EC2StartStop tools");
-			log.exit();
+			LOG.info("End of EC2StartStop tools");
+			LOG.exit();
 		}
 		catch (final Exception e) {
-			log.error("Error during EC2StartStop tools execution", e);
-			log.exit(e);
+			LOG.error("Error during EC2StartStop tools execution", e);
+			LOG.exit(e);
 			throw e;
 		}
 	}
