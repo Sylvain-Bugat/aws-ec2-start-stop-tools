22f543c00bd2b80de05d6c7201de0e39a06297aa
Sylvain Bugat <sylvain.bugat@laposte.net>
2015-04-11 20:14:07 +0200

    Add comments

--- src/main/java/com/github/sbugat/ec2tools/model/instance/OrderType.java
+++ src/main/java/com/github/sbugat/ec2tools/model/instance/OrderType.java
@@ -1,12 +1,14 @@
 package com.github.sbugat.ec2tools.model.instance;
 
 /**
- * Possible instance order type
- *
+ * Possible instance order type.
+ * 
  * @author Sylvain Bugat
  */
 public enum OrderType {
 
+	/** Start order type. */
 	START,
+	/** stop order type. */
 	STOP;
 }
