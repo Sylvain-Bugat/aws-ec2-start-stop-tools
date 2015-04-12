e30005733140ff6122b66ae863a8d405afba8c2a
Sylvain Bugat <sylvain.bugat@laposte.net>
2015-04-12 13:06:36 +0200

    Change equals/hashcode tests

--- src/test/java/com/github/sbugat/ec2tools/service/options/ProgramOptionsTest.java
+++ src/test/java/com/github/sbugat/ec2tools/service/options/ProgramOptionsTest.java
@@ -2,6 +2,8 @@ package com.github.sbugat.ec2tools.service.options;
 
 import java.util.ArrayList;
 
+import nl.jqno.equalsverifier.EqualsVerifier;
+
 import org.assertj.core.api.Assertions;
 import org.assertj.core.util.Lists;
 import org.junit.Test;
@@ -38,72 +40,8 @@ public class ProgramOptionsTest {
 	}
 
 	@Test
-	public void testProgramOptionsEqualsSameObject() {
+	public void testEqualsAndHashCode() {
 
-		final ProgramOptions programOptions = new ProgramOptions(false, false, false, false, new ArrayList<String>());
-		Assertions.assertThat(programOptions).isEqualTo(programOptions);
-	}
-
-	@Test
-	public void testProgramOptionsEquals() {
-
-		final ProgramOptions programOptions = new ProgramOptions(false, true, false, true, Lists.newArrayList(SECTION_1, SECTION_2));
-		Assertions.assertThat(programOptions).isEqualTo(new ProgramOptions(false, true, false, true, Lists.newArrayList(SECTION_1, SECTION_2)));
-	}
-
-	@Test
-	public void testProgramOptionsEqualsNull() {
-
-		final ProgramOptions programOptions = new ProgramOptions(false, false, false, false, new ArrayList<String>());
-		Assertions.assertThat(programOptions).isNotEqualTo(null);
-	}
-
-	@Test
-	public void testProgramOptionsEqualsDifferentType() {
-
-		final ProgramOptions programOptions = new ProgramOptions(false, false, false, false, new ArrayList<String>());
-		Assertions.assertThat(programOptions).isNotEqualTo(SECTION_1);
-	}
-
-	@Test
-	public void testProgramOptionsEqualsDifferentExecutionFlag() {
-
-		final ProgramOptions programOptions = new ProgramOptions(false, false, false, false, Lists.newArrayList(SECTION_1, SECTION_2));
-		Assertions.assertThat(programOptions).isNotEqualTo(new ProgramOptions(true, false, false, false, Lists.newArrayList(SECTION_1, SECTION_2)));
-	}
-
-	@Test
-	public void testProgramOptionsEqualsDifferentListFlag() {
-
-		final ProgramOptions programOptions = new ProgramOptions(false, false, false, false, Lists.newArrayList(SECTION_1, SECTION_2));
-		Assertions.assertThat(programOptions).isNotEqualTo(new ProgramOptions(false, true, false, false, Lists.newArrayList(SECTION_1, SECTION_2)));
-	}
-
-	@Test
-	public void testProgramOptionsEqualsDifferentCheckFlag() {
-
-		final ProgramOptions programOptions = new ProgramOptions(false, false, false, false, Lists.newArrayList(SECTION_1, SECTION_2));
-		Assertions.assertThat(programOptions).isNotEqualTo(new ProgramOptions(false, false, true, false, Lists.newArrayList(SECTION_1, SECTION_2)));
-	}
-
-	@Test
-	public void testProgramOptionsEqualsDifferentPostcheckFlag() {
-
-		final ProgramOptions programOptions = new ProgramOptions(false, false, false, false, Lists.newArrayList(SECTION_1, SECTION_2));
-		Assertions.assertThat(programOptions).isNotEqualTo(new ProgramOptions(false, false, false, true, Lists.newArrayList(SECTION_1, SECTION_2)));
-	}
-
-	@Test
-	public void testProgramOptionsEqualsDifferentSections() {
-
-		final ProgramOptions programOptions = new ProgramOptions(false, false, false, false, Lists.newArrayList(SECTION_1, SECTION_2));
-		Assertions.assertThat(programOptions).isNotEqualTo(new ProgramOptions(false, false, false, true, Lists.newArrayList(SECTION_1)));
-	}
-
-	@Test
-	public void testProgramOptionsEqualsDifferentSectionsOrder() {
-
-		final ProgramOptions programOptions = new ProgramOptions(false, false, false, false, Lists.newArrayList(SECTION_1, SECTION_2));
-		Assertions.assertThat(programOptions).isNotEqualTo(new ProgramOptions(false, false, false, true, Lists.newArrayList(SECTION_2, SECTION_1)));
+		EqualsVerifier.forClass(ProgramOptions.class).verify();
 	}
 }
